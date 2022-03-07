e2825f119fcf29fe367569e40e75fb65b16153a8
semyoon17 <96520411+semyoon17@users.noreply.github.com>
2022-03-07 15:14:51 +0400

    kamyshev_semen_dz_3 commit

---
+++ kamyshev_semen_dz_3/3.1.py
@@ -0,0 +1,10 @@
+def num_translate(a):
+    list_num = ['Ноль', 'Один', 'Два', 'Три', 'Четыре', 'Пять', 'Шесть', 'Семь', 'Восемь', 'Девять', 'Десять']
+    for i, s in enumerate(list_num):
+        if a == i:
+            print(s)
+
+
+num_translate(6)
+
+
