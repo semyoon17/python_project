bea053e0a7f971c206cd0afdb7f4d40a77429223
semyoon17 <96520411+semyoon17@users.noreply.github.com>
2022-04-03 18:46:07 +0400

    Update show_sales.py

--- kamyshev_semen_dz_6/show_sales.py
+++ kamyshev_semen_dz_6/show_sales.py
@@ -2,15 +2,18 @@ import sys
 
 with open('sales.txt', 'r', encoding='UTF-8') as f:
     sales = f.readlines()
-    if len(sys.argv) == 1:
-        for line in sales:
-            print(line)
-    elif len(sys.argv) == 2:
-        print(*sales[int(sys.argv[1]):])
-    elif len(sys.argv) == 3:
-        print(*sales[int(sys.argv[1])-1:int(sys.argv[2])])
-    elif len(sys.argv) >= 4:
-        print('Введи не больше 2 значений')
+    try:
+        if len(sys.argv) == 1:
+            for line in sales:
+                print(line)
+        elif len(sys.argv) == 2:
+            print(*sales[int(sys.argv[1]):])
+        elif len(sys.argv) == 3:
+            print(*sales[int(sys.argv[1])-1:int(sys.argv[2])])
+        elif len(sys.argv) >= 4:
+            print('Введи не больше 2 значений')
+    except ValueError:
+        print('Принимаются только числовые значения')
 
 
 
